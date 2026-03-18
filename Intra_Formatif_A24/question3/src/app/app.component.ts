bff3d543a67da06a8b70c8ae65c5b6e903ec52a8
Sofiane Semid <6240792@cegepmontpetit.ca>
2026-03-18 17:11:23 -0400

    rshfbg

--- Intra_Formatif_A24/question3/src/app/app.component.ts
+++ Intra_Formatif_A24/question3/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,20 +9,77 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: true,
-    imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule]
 })
 export class AppComponent {
   title = 'reactive.form';
 
-  
 
-  constructor(
-    
-  ) { }
+  formGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+
+    this.formGroup = this.formBuilder.group(
+      {
+        nom: ['', [Validators.required]],
+        numRue: ['', [
+          Validators.required,
+          Validators.min(1000),
+          Validators.max(9999)
+        ]],
+        codePostal: ['', [Validators.pattern("^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$")]],
+        commentaire: ['', [
+         
+          this.minimumMots,
+        ]],
+      },
+      {
+        validators: this.nomDansCommentaireValidator
+
+      }
+      
+    )
+  }
+
+  minimumMots(control: AbstractControl): ValidationErrors | null {
+    const texte = control.value
+
+    if (!texte) {
+      return null
+    }
+    const mots = texte.split(' ');
+
+    if (mots.length < 10) {
+      return { minMots: true };
+    }
+    return null
+  }
+
+   nomDansCommentaireValidator(control: AbstractControl): ValidationErrors | null {
+
+  const nom = control.get('nom')?.value;
+  const commentaire = control.get('commentaire')?.value;
+
+  if (!nom || !commentaire) return null;
+
+  if (commentaire.toLowerCase().includes(nom.toLowerCase())) {
+
+    control.get('commentaire')?.setErrors({
+      nomDansCommentaire: true
+    });
+
+  }
+
+  return null;
+}
+
+
+
+
+
 }
 
 
